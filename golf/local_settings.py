d17f6ada2eb7ca81a9655fca350b729cc0fa0d2d
Ayush Singh <ayush.singh@niser.ac.in>
2019-06-15 22:53:02 +0530

    still trying to deploy

--- golf/local_settings.py
+++ golf/local_settings.py
@@ -30,6 +30,5 @@ USE_L10N = True
 USE_TZ = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+import django_heroku
+django_heroku.settings(locals())
