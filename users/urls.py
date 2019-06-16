2f11b38c352fd75049f9e08ecc607809c815e569
Ayush Singh <ayush.singh@niser.ac.in>
2019-06-16 12:23:46 +0530

    fixed logout

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from .views import *
@@ -7,6 +7,7 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
     path('login/', LoginView.as_view(template_name='login.htm'), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
     path('update/', UserUpdateView.as_view(), name='update'),
     path('<slug:pk>/', UserView.as_view(), name='profile'),
 ]
